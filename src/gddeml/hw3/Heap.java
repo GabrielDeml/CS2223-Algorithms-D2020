7bcde1019bc2def710adc9c6baf5f2853ec76285
Gabe Deml <gddeml@wpi.edu>
2020-04-26 13:40:33 -0400

    checkpoint

---
+++ src/gddeml/hw3/Heap.java
@@ -0,0 +1,96 @@
+package gddeml.hw3;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * The {@code Heap} class provides a static method to constructHeap.
+ * <p>
+ * This is the first step in HeapSort, and all that is being considered for
+ * Question 1.
+ * <p>
+ * You will need to modify this class to count # of exchanges,
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ * @modified George Heineman
+ */
+public class Heap {
+	private static int numberOfExchanges = 0;
+	private static int numberOfCompares = 0;
+
+
+	public static int getNumberOfCompares() {
+		return numberOfCompares;
+	}
+
+	public static int getNumberOfExchanges() {
+		return numberOfExchanges;
+	}
+
+	public static void resetExchangesAndCompares() {
+		numberOfCompares = 0;
+		numberOfExchanges = 0;
+	}
+
+
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     *
+     * @param a the array to be sorted
+     */
+    public static void constructHeap(Comparable[] a) {
+        int n = a.length;
+
+        // construct heap from the raw array of which we know nothing.
+        // This is the focus of Question 1 on the homework.
+        for (int k = n / 2; k >= 1; k--) {
+            sink(a, k, n);
+        }
+
+        // at this point, a has been turned into a heap.
+        // For this question, we are not concerned with fully sorting...
+        // while (n > 1) {
+        //	 exch(a, 1, n--);
+        //	 sink(a, 1, n);
+        // }
+    }
+
+    /***************************************************************************
+     * Helper functions to restore the heap invariant.
+     ***************************************************************************/
+
+    private static void sink(Comparable[] pq, int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(pq, j, j + 1)) j++;
+            if (!less(pq, k, j)) break;
+            exch(pq, k, j);
+            k = j;
+        }
+    }
+
+    /***************************************************************************
+     * Helper functions for comparisons and swaps.
+     * Indices are "off-by-one" to support 1-based indexing.
+     ***************************************************************************/
+    private static boolean less(Comparable[] pq, int i, int j) {
+		numberOfCompares++;
+		return pq[i - 1].compareTo(pq[j - 1]) < 0;
+    }
+
+    private static void exch(Object[] pq, int i, int j) {
+        Object swap = pq[i - 1];
+        pq[i - 1] = pq[j - 1];
+        pq[j - 1] = swap;
+		numberOfExchanges++;
+    }
+
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+}
---
+++ src/gddeml/hw3/Question1.java
@@ -0,0 +1,52 @@
+package gddeml.hw3;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This is the template code for question 1.
+ *
+ * Be sure to Explain whether the empirical results support the proposition.
+ *
+ */
+public class Question1 {
+	public static void main(String[] args) {
+		System.out.println("N      MaxComp       MaxEach");
+		for (int i = 16; i <= 512; i *= 2) {
+			Double[] compareableList = new Double[i];
+			int maxNumberOfExchanges = 0;
+			int maxNumberOfCompares = 0;
+			for (int numOfTrials = 0; numOfTrials < 100; numOfTrials++) {
+				for (int n = 0; n < i; n++) {
+					compareableList[n] = (StdRandom.uniform());
+				}
+				Heap.constructHeap(compareableList);
+				if (Heap.getNumberOfExchanges() > maxNumberOfExchanges) {
+					maxNumberOfExchanges = Heap.getNumberOfExchanges();
+				}
+				if (Heap.getNumberOfCompares() > maxNumberOfCompares) {
+					maxNumberOfCompares = Heap.getNumberOfCompares();
+				}
+				Heap.resetExchangesAndCompares();
+			}
+			System.out.println(i + "       " + maxNumberOfCompares + "        " + maxNumberOfExchanges);
+		}
+
+		// for N in 16 .. 512
+		
+		//   for each N, do T=100 trials you want to keep track of 
+		//       what you believe to be the MOST number of exch invocations
+		//       and most number of less invocations
+		
+		//       compute a random array of N uniform doubles
+		
+		//   Make sure you output for each N the maximum values you saw
+		//   in a table like...
+		//
+		//       N   MaxComp    MaxExch
+		//       16  22         8
+		//     .....
+	}
+}
